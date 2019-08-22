76fe26fd28240fbb02305c2c92d620062d3e603d
Caleb Bauermeister <cbauerman@Calebs-iMac.local>
2019-08-22 10:13:05 -0700

    Work on authorizing request to wpcom

--- _inc/lib/class.core-rest-api-endpoints.php
+++ _inc/lib/class.core-rest-api-endpoints.php
@@ -84,7 +84,7 @@ class Jetpack_Core_Json_Api_Endpoints {
 		register_rest_route( 'jetpack/v4', 'marketing/survey', array(
 			'methods'             => WP_REST_Server::CREATABLE,
 			'callback'            => __CLASS__ . '::submit_survey',
-			// 'permission_callback' => __CLASS__ . '::connect_url_permission_callback',
+			'permission_callback' => __CLASS__ . '::unlink_user_permission_callback',
 
 		) );
 
@@ -497,10 +497,11 @@ class Jetpack_Core_Json_Api_Endpoints {
 			array(
 				'method'  => 'POST',
 				'headers' => array(
+					'Content-Type'    => 'application/json',
 					'X-Forwarded-For' => Jetpack::current_user_ip( true ),
 				),
 			),
-			[],
+			$request->get_body(),
 			'rest'
 		);
 
@@ -513,9 +514,6 @@ class Jetpack_Core_Json_Api_Endpoints {
 		}
 
 		return $data;
-		// return $request;
-		// return new WP_Error( 'forbidden', __( 'Site endpoint is under construction.', 'jetpack' ) );
-		// return false;
 	}
 
 	/**
